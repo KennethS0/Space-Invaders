a5dd521e53294450580cab2e1d8e3f066071dcd4
Francisco Badilla <francisco.badilla.fonseca@gmail.com>
2020-08-05 12:37:43 -0600

    Testing Player Movement in SFML

--- src/view/GameWindow.cpp
+++ src/view/GameWindow.cpp
@@ -14,6 +14,8 @@ using namespace std;
 #define Screen_Width 900
 #define Screen_Height 600
 
+#define Sprite_Speed 1
+
 
 
 void Game::run()
@@ -21,12 +23,25 @@ void Game::run()
     sf::RenderWindow gameWindow(sf::VideoMode(Screen_Width, Screen_Height), "Space Invaders");
     sf::Texture EnemyImage;
     sf::Texture PlayerImage;
-    if(!EnemyImage.loadFromFile("Player.png")){
+    if(!PlayerImage.loadFromFile("Player.png")){
         std::cout << "Error Loading Image!" << std::endl;
     }
     if (!EnemyImage.loadFromFile("Enemy.png")){
         std::cout << "Error Loading Image!" << std::endl;
     }
+    EnemyImage.setSmooth(true);
+    PlayerImage.setSmooth(true);
+
+
+    bool Up = false;
+    bool Down = false;
+    bool Left = false;
+    bool Right = false;
+
+    int x;
+    int y;
+
+
     sf::Sprite Enemy1(EnemyImage);
     sf::Sprite Enemy2(EnemyImage);
     sf::Sprite Enemy3(EnemyImage);
@@ -48,12 +63,13 @@ void Game::run()
     Enemy2.setScale(ScaleX, ScaleY);
     Enemy3.setScale(ScaleX, ScaleY);
     Enemy4.setScale(ScaleX, ScaleY);
-
+    Player.setScale(ScaleX, ScaleY);
     
-    Enemy2.setPosition(60,0);
-    Enemy3.setPosition(120,0);
-    Enemy4.setPosition(180,0);
-    Player.setPosition(450, 500);
+    Enemy1.setPosition(100,0);
+    Enemy2.setPosition(160,0);
+    Enemy3.setPosition(220,0);
+    Enemy4.setPosition(280,0);
+
 
     
 
@@ -66,8 +82,47 @@ void Game::run()
         {
             if (event.type == sf::Event::Closed)
                 gameWindow.close();
+            if (event.type == sf::Event::KeyPressed){
+                switch(event.key.code){
+                    case sf::Keyboard::W:
+                        Up = true;
+                        break;
+                    case sf::Keyboard::S:
+                        Down = true;
+                        break;
+                    case sf::Keyboard::A:
+                        Left = true;
+                        break;
+                    case sf::Keyboard::D:
+                        Right = true;
+                        break;
+                }
+            }
+
+            if (event.type == sf::Event::KeyReleased){
+                switch(event.key.code){
+                    case sf::Keyboard::W:
+                        Up = false;
+                        break;
+                    case sf::Keyboard::S:
+                        Down = false;
+                        break;
+                    case sf::Keyboard::A:
+                        Left = false;
+                        break;
+                    case sf::Keyboard::D:
+                        Right = false;
+                        break;      
+                }
+            }
         }  
 
+       // Update coordinates
+        if (Left) x-=Sprite_Speed;
+        if (Right) x+=Sprite_Speed;
+        if (Up) y-=Sprite_Speed;
+        if (Down) y+=Sprite_Speed;
+    Player.setPosition(x, y);
     //Window actions
     gameWindow.clear();
     gameWindow.draw(Enemy1);
