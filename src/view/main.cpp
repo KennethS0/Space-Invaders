cde9170f40c3e62945c56dd076a80c9b33037553
Fabio <fabiojosue16@gmail.com>
2020-08-05 01:58:59 -0600

    base interface

--- src/view/main.cpp
+++ src/view/main.cpp
@@ -19,38 +19,47 @@ int main()
 {
     
     sf::RenderWindow window(sf::VideoMode(Screen_Width, Screen_Height), "Space Invaders");
-    sf::Texture EnemyImage;
-    if (!EnemyImage.loadFromFile("Enemy.png")){
-        std::cout << "Error Loading Image!" << std::endl;
-    }
-    sf::Sprite Enemy1(EnemyImage);
-    sf::Sprite Enemy2(EnemyImage);
-    sf::Sprite Enemy3(EnemyImage);
-    sf::Sprite Enemy4(EnemyImage);
-
+    sf::Texture BackgroundTexture;
+    sf::Sprite background;
 
     sf::Vector2u sizeOfWindow;
     sf::Vector2u sizeOfTexture;
+
+    sf::Font MyFont;
+    if(!MyFont.loadFromFile("SPACEMAN.TTF")) return -1;
+
+    sf::Text title;
+    title.setFont(MyFont);
+    title.setCharacterSize(55);
+    title.setStyle(sf::Text::Bold);
+    title.setString("SPACE INVADERS");
+    title.setPosition(sf::Vector2f(80,80));
+
+    sf::Text subTitle;
+    subTitle.setFont(MyFont);
+    subTitle.setCharacterSize(15);
+    //subTitle.setStyle(sf::Text::Bold);
+    subTitle.setString("Hecho por Fabio, Francisco y Kenneth");
+    subTitle.setPosition(sf::Vector2f(180,150));
     
+    if(!BackgroundTexture.loadFromFile("background.jpg")){
+        return -1;
+    } else{
+        sizeOfTexture = BackgroundTexture.getSize();
+        sizeOfWindow = window.getSize();
+
+        float ScaleX = (float) sizeOfWindow.x / sizeOfTexture.x;
+        float ScaleY = (float) sizeOfWindow.y / sizeOfTexture.y;
+
+        background.setTexture(BackgroundTexture);
+        background.setScale(ScaleX, ScaleY);
+    }
 
     //Scaling Functionalities
     sizeOfWindow = window.getSize();
 
     float ScaleX = 0.1;
 	float ScaleY = 0.1; 
-
-    Enemy1.setScale(ScaleX, ScaleY);
-    Enemy2.setScale(ScaleX, ScaleY);
-    Enemy3.setScale(ScaleX, ScaleY);
-    Enemy4.setScale(ScaleX, ScaleY);
-
-    
-    Enemy2.setPosition(60,0);
-    Enemy3.setPosition(120,0);
-    Enemy4.setPosition(180,0);
-
-
-
 
     //Main Window Loop
     while (window.isOpen())
@@ -64,10 +73,9 @@ int main()
 
     //Window actions
     window.clear();
-    window.draw(Enemy1);
-    window.draw(Enemy2);
-    window.draw(Enemy3);
-    window.draw(Enemy4);
+    window.draw(background);
+    window.draw(title);
+    window.draw(subTitle);
     window.display();
     }
     return 0;
