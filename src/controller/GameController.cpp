606693f1811ddba3a88bce52f0472e63eda7c1c8
KennethS0 <kenneth.sanchez0609@gmail.com>
2020-08-10 00:21:12 -0600

    Input detected from controller

--- src/controller/GameController.cpp
+++ src/controller/GameController.cpp
@@ -17,23 +17,53 @@ GameController::GameController() {
     Game model = Game();
     GameWindow view = GameWindow();
 
+    bool gameStarted = false;
+
     view.loadMenu(si_window);
 
     while (si_window.isOpen()){
         sf::Event event;
+
         while (si_window.pollEvent(event)) {
+
             if (event.type == sf::Event::Closed) si_window.close();
 
-            if (event.type == sf::Event::KeyPressed) {
-                // ESC
-                if (event.key.code == sf::Keyboard::Escape) {
-                    si_window.close();
-                }
-                // ENTER (LOADS NEW WINDOW)
-                else if (event.key.code == sf::Keyboard::Return) {
-                    view.loadGame(si_window);
-                    si_window.display();
+            if (!gameStarted) {
+                // Menu options
+                if (event.type == sf::Event::KeyPressed) {
+                    // ESC
+                    if (event.key.code == sf::Keyboard::Escape) {
+                        si_window.close();
+                    }
+                    // ENTER (LOADS NEW WINDOW)
+                    else if (event.key.code == sf::Keyboard::Return) {
+                        view.loadGame(si_window);
+                        si_window.display();
 
+                        model.setOver(false);
+                        gameStarted = true;
+                        model.startGame(false);
+                    }
+                }
+            } else {
+                // Game logic and redrawing
+                if (event.type == sf::Event::KeyPressed) {
+                    
+                    // Right movement
+                    if (event.key.code == sf::Keyboard::A) {
+                        std::cout<<"A"<<endl;
+                    } else if (event.key.code == sf::Keyboard::D) {
+                        std::cout<<"D"<<endl;
+                    } else if (event.key.code == sf::Keyboard::Space) {
+                        std::cout<<"Space"<<endl;
+                    } else if (event.key.code == sf::Keyboard::Escape) {
+                        view.loadMenu(si_window);
+                        si_window.display();
+
+                        // Ends game and shows menu
+                        gameStarted = false;
+                        model.setOver(true);
+                    }
                 }
             }
         }
